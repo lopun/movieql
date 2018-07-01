f7473e07f03e6d02ac08ef389c769d9b475f2c0e
ujin43255252 <ujin43255252@gmail.com>
2018-07-01 23:42:43 +0900

    yts.ag api added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // nodemon 덕분에 index.js 파일이 항상 실행되게 된다.
+
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
+
+const server = new GraphQLServer({
+  typeDefs: "graphql/schema.graphql",
+  resolvers
+});
+
+server.start(() => console.log("GraphQL Server running!"));
